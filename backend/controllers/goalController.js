7b7f712a1a2c891625419e6fdf907db5d1e84c9a
Trevor Lister <Trevor Lister>
2022-06-27 22:01:08 -0300

    REST Api for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,11 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 const getGoals = asyncHandler (async (req, res) => { 
-  res.status(200).json({ message: 'Get goals'})
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
 })
 
 const setGoal = asyncHandler (async (req, res) => { 
@@ -9,15 +13,40 @@ const setGoal = asyncHandler (async (req, res) => {
     res.status(400)
     throw new Error('Please add a text field.')
   }
-  res.status(200).json({ message: 'Set goal'})
+
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+  res.status(200).json(goal)
 })
 
 const updateGoal = asyncHandler (async (req, res) => { 
-  res.status(200).json({ message: `Update goal ${req.params.id}`})
+  // We use params because it is going to be in the url
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  // (id, data, options)
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
+    new: true,
+  })
+  res.status(200).json(updatedGoal)
 })
 
 const deleteGoal = asyncHandler (async (req, res) => { 
-  res.status(200).json({ message: `Delete goal ${req.params.id}`})
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
